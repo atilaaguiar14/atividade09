afca003d8e9f775b075b7497df2e5c36bfd685f0
atilasousaa285 <atilaaguiarsousa285@gmail.com>
2023-10-23 14:12:31 -0300

    first commit

---
+++ software.py
@@ -0,0 +1,7 @@
+# Faça um software que verifique entre 2 numeros qual o maior
+n1 = int(input("digite um numero 01: "))
+n2 = int(input("digite um numero 02: "))
+if n1 > n2:
+ print(n1)
+else:
+print(n2)
